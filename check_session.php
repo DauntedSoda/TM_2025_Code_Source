46015a5c33348a971a4b4eb75801a83cf4cbb6e2
DauntedSoda <108807210+DauntedSoda@users.noreply.github.com>
2025-11-02 21:42:01 +0100

    initial commit n+1

--- check_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "functions.php";
-start_secure_session();
-
-if (isset($_SESSION['id'])) {
-    echo "Logged in as user ID: " . $_SESSION['id'];
-} else {
-    echo "Not logged in.";
-}
